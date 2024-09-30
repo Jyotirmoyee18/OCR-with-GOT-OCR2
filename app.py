3a9c01e0cd056d06eb27452c6f912762bd88dfb6
Jyotirmoyee Mandal <149067724+Jyotirmoyee18@users.noreply.github.com>
2024-09-30 13:46:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ import re
 reader = easyocr.Reader(['en', 'hi'])  # 'en' for English, 'hi' for Hindi
 
 # Load the GOT-OCR2 model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True)
-model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
+tokenizer = AutoTokenizer.from_pretrained('stepfun-ai/GOT-OCR2_0', trust_remote_code=True)
+model = AutoModel.from_pretrained('stepfun-ai/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
 model = model.eval().cuda()
 
 # Load MarianMT translation model for Hindi to English translation
